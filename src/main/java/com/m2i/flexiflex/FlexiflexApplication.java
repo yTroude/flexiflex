5a216d065285c2dca8870606e1c2b0b8a1752cb8
benoit <benoitzuing@msn.com>
2018-10-30 16:40:17 +0100

    initialisation

---
+++ src/main/java/com/m2i/flexiflex/FlexiflexApplication.java
@@ -0,0 +1,12 @@
+package com.m2i.flexiflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexiflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexiflexApplication.class, args);
+	}
+}
