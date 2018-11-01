6c3bf12bb8381ea591d0da3f253be1dda2656f4b
benoit <benoitzuing@msn.com>
2018-11-01 15:18:25 +0100

    deploiement jenkins

--- src/main/java/com/m2i/flexiflex/FlexiflexApplication.java
+++ src/main/java/com/m2i/flexiflex/FlexiflexApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlexiflexApplication {
 
-    private static String username = "benoit";
-    private static String password = "secret";
-
 	public static void main(String[] args) {
 		SpringApplication.run(FlexiflexApplication.class, args);
         System.out.println("Hello world!");
