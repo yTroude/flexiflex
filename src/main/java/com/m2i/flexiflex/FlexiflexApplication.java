483e93c2a1b852d0103439c4c3334da4cf935869
benoit <benoitzuing@msn.com>
2018-11-01 19:47:58 +0100

    ajout <distribution management> dans pom.xml

--- src/main/java/com/m2i/flexiflex/FlexiflexApplication.java
+++ src/main/java/com/m2i/flexiflex/FlexiflexApplication.java
@@ -8,6 +8,5 @@ public class FlexiflexApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlexiflexApplication.class, args);
-        System.out.println("Hello world!");
 	}
 }
