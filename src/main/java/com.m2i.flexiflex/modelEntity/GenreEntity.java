1e69de7b9ecb654ac00a8d12f3429c4673964226
fraktalobo <loic.bourg@gmail.com>
2018-11-07 14:46:44 +0100

    Configuration Hibernate ajoutée

---
+++ src/main/java/com.m2i.flexiflex/modelEntity/GenreEntity.java
@@ -0,0 +1,50 @@
+package com.m2i.flexiflex.modelEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "genre", schema = "flexiflex", catalog = "")
+public class GenreEntity {
+    private int id;
+    private String genre;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "genre")
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GenreEntity that = (GenreEntity) o;
+
+        if (id != that.id) return false;
+        if (genre != null ? !genre.equals(that.genre) : that.genre != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (genre != null ? genre.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/com.m2i.flexiflex/modelEntity/IdentityEntity.java
@@ -0,0 +1,63 @@
+package com.m2i.flexiflex.modelEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "identity", schema = "flexiflex", catalog = "")
+public class IdentityEntity {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "firstName")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "lastName")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        IdentityEntity that = (IdentityEntity) o;
+
+        if (id != that.id) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
+}
