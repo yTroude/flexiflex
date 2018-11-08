a08f77318bcf944a68fbeada5d0b634c96775a11
David Zanuttini <david.zanuttini@live.fr>
2018-11-08 14:56:46 +0100

    Cryptor: cryptage d'une string en SHA-256, et comparaison d'une string avec un hash deja crypte.

---
+++ src/main/java/com.m2i.flexiflex/service/Cryptor.java
@@ -0,0 +1,27 @@
+package com.m2i.flexiflex.service;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+public class Cryptor {
+
+    private static String salt="D36B32H34F31L31Y30";
+
+    public static String getSHA256(String input){
+
+        String toReturn = null;
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.reset();
+            messageDigest.update((input+salt).getBytes("utf8"));
+            toReturn = String.format("%040x", new BigInteger(1, messageDigest.digest()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return toReturn;
+    }
+
+    public static boolean verifySHA256(String input, String hash) {
+        return hash.equals(getSHA256(input));
+    }
+}
