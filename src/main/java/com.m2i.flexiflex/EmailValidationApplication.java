2643259aa3a93209ea497d42f21d77a2bb910995
David Zanuttini <david.zanuttini@live.fr>
2018-11-08 15:05:57 +0100

    EmailValidation: Lecture et vérification des paramètres du controller, correspondant au lien envoyé dans l'email de validation de l'inscription.

---
+++ src/main/java/com.m2i.flexiflex/EmailValidationApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.flexiflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailValidationApplication.class, args);
+    }
+}
+
