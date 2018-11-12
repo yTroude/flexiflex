7b091083dc245217d85d76586fd4fadd01410932
arte-fact <benoitzuing@msn.com>
2018-11-12 15:00:35 +0100

    merge develop

--- src/main/java/com.m2i.flexiflex/controller/AuthController.java
+++ src/main/java/com.m2i.flexiflex/controller/AuthController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class AuthController {
 
