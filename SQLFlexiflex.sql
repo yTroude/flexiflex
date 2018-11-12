6e7bc70a2aa7b0fe294bfbb5a39c4e85dcd59522
fraktalobo <loic.bourg@gmail.com>
2018-11-12 16:19:22 +0100

    attributes added to user table

--- SQLFlexiflex.sql
+++ SQLFlexiflex.sql
@@ -92,10 +92,13 @@
 -- Export de la structure de la table flexiflex. utilisateur
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL UNIQUE,
   `password` char(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
+  `inscriptionDate` DATE NOT NULL,
+  `validationToken` char(64) NOT NULL,
+  `emailValidation` int DEFAULT 0 ,
+  `UUID` char(36) NOT NULL UNIQUE,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 #
 # -- Les données exportées n'étaient pas sélectionnées.
