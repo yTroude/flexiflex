cfa259fe9f24095da84231ad3ffd5980354529ab
arte-fact <benoitzuing@msn.com>
2018-11-29 12:40:32 +0100

    Clean : ignore

--- src/test/java/com.m2i.flexiflex/controller/AuthControllerTest.java
+++ src/test/java/com.m2i.flexiflex/controller/AuthControllerTest.java
@@ -11,6 +11,7 @@ import org.hibernate.criterion.Property;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
