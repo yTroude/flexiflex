cf6fa6dde1950e316aa2624971ad16a09fd85df7
arte-fact <benoitzuing@msn.com>
2018-11-29 15:11:54 +0100

    refacto: suppression d'import inutiles dans

--- src/main/java/com.m2i.flexiflex/FlexiflexApplication.java
+++ src/main/java/com.m2i.flexiflex/FlexiflexApplication.java
@@ -1,9 +1,7 @@
 package com.m2i.flexiflex;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class FlexiflexApplication {
