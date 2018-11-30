5fc9147a41ba82cc91aae7297c5f3610651aafe2
arte-fact <benoitzuing@msn.com>
2018-11-30 11:27:10 +0100

    implementation dans model, controller, service et test 2

--- src/test/java/com.m2i.flexiflex/controller/AuthControllerTest.java
+++ src/test/java/com.m2i.flexiflex/controller/AuthControllerTest.java
@@ -1,7 +1,9 @@
 package com.m2i.flexiflex.controller;
 
 import com.m2i.flexiflex.controller.factories.UserFactory;
+import com.m2i.flexiflex.entity.UserEntity;
 import com.m2i.flexiflex.entity.properties.UserProperties;
+import com.m2i.flexiflex.service.Cryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.servlet.http.Cookie;
 import java.nio.charset.Charset;
 
 import static com.m2i.flexiflex.controller.factories.UserFactory.deleteTestUser;
@@ -17,6 +20,7 @@ import static com.m2i.flexiflex.controller.factories.UserFactory.makeTestUser;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -55,13 +59,21 @@ public class AuthControllerTest {
 
     @Test
     public void userCanLoginWithPassword() throws Exception {
-        makeTestUser();
+        UserEntity user = makeTestUser();
+
+        String hash = Cryptor.get_SHA_512(user.getUuid(), "489864zge84zh86h7z68h4z6e");
+        String value = user.getUuid() + "_" + hash;
+        Cookie myCookie = new Cookie("flexiflex", user.getUuid() + "_" + hash);
 
         mvc.perform(post("/login")
                 .param(UserProperties.EMAIL, UserFactory.getTestUserMail())
                 .param(UserProperties.PASSWORD, UserFactory.getTestUserPassword())
                 .contentType(APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk()).andDo(print());
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        assert myCookie.getName().equals("flexiflex");
+        assert myCookie.getValue().equals(value);
 
         deleteTestUser();
     }
